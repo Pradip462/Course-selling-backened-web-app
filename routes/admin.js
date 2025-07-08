3d223cd902f6d1923ca7897aa6ce90ab3bf4c4bb
Pradip462 <pradipkumarmukhi462@gmail.com>
2025-07-08 16:27:45 +0530

    Added signup logic in the admin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,108 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { AdminModel } = require("../db");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const z = require("zod");
+const { parse } = require("dotenv");
+const { email } = require("zod/v4-mini");
+require("dotenv").config();
 
-adminRouter.post("/signup",(req,res) => {
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
-})
+// Admin Auth
+const adminAuth = async () => {};
 
-adminRouter.post("/signin",(req,res) => {
+adminRouter.post("/signup", async (req, res) => {
+  const zodBody = z.object({
+    firstName: z
+      .string()
+      .min(3, { message: "First Name should minimun of 3 length" })
+      .max(50, { message: "First Name Should maximum of 50 length" }),
+    lastName: z
+      .string()
+      .min(3, { message: "Last Name should minimun of 3 length" })
+      .max(50, { message: "Last Name Should maximum of 50 length" }),
+    email: z
+      .string()
+      .min(3, { message: "Email should minimun of 3 length" })
+      .max(50, { message: "Email Should maximum of 50 length" })
+      .email("The input should be email"),
+    password: z
+      .string()
+      .min(8, { message: "Password should minimun of 8 length" })
+      .max(30, { message: "Password Should maximum of 50 length" })
+      .refine((val) => /[A-Z]/.test(val), {
+        message: "Password must contain one Uppercase",
+      })
+      .refine((val) => /[a-z]/.test(val), {
+        message: "Password must contain one Lowercase",
+      })
+      .refine((val) => /[0-9]/.test(val), {
+        message: "Password must contain one Number",
+      })
+      .refine((val) => /[!@#$%^&*]/.test(val), {
+        message: "Password must contain one Special Character",
+      }),
+  });
 
-})
+  const parsedDataWithSuccess = zodBody.safeParse(req.body);
+
+  if (!parsedDataWithSuccess.success) {
+    res.status().json({
+      msg: "Incorrect Input Pattern",
+      error: parsedDataWithSuccess.error,
+    });
+    return;
+  }
+
+  const { firstName, lastName, email, password } = req.body;
+
+  const existUser = AdminModel.findOne({
+    email: email,
+  });
+
+  if (existUser) {
+    res.status(403),
+      json({
+        msg: "Admin Already exists in the database",
+      });
+    return;
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await AdminModel.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(200).json({
+      msg: "Successfully Signed Up",
+    });
+  } catch (err) {
+    console.log(`Error occuring during Signup : ${err}`);
+    res.status(500).json({
+      msg: "Internal Server Error during SignUp",
+    });
+  }
+});
+
+adminRouter.post("/signin", (req, res) => {});
 
 // admin can create a course
-adminRouter.post("/course",(req,res) => {
-
-})
+adminRouter.post("/course", (req, res) => {});
 
 // change the course features like name,pricing
-adminRouter.put("/course",(req,res) => {
-
-})
+adminRouter.put("/course", (req, res) => {});
 
 // get all the course
-adminRouter.put("/course/all",(req,res) => {
-
-})
-
+adminRouter.put("/course/all", (req, res) => {});
 
 module.exports = {
-    adminRouter
-}
+  adminRouter,
+};
